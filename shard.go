8339e6474f194f9c00e48b9301340c909b1904bf
Robin Verlangen <robin@us2.nl>
2015-12-16 23:11:43 +0100

    Move logic from shard writer to shard, as the shard writer is something else

--- shard.go
+++ shard.go
@@ -98,6 +98,55 @@ func (this *Shard) SetContents(b *bytes.Buffer) {
 	this.contentsMux.Unlock()
 }
 
+// Add file
+func (this *Shard) AddFile(f *FileMeta, b []byte) (*FileMeta, error) {
+	// Validate mode
+	this.bufferModeMux.Lock()
+	if this.bufferMode == ReadShardBufferMode {
+		// Reading..
+		panic("Shard is reading, can not write")
+	}
+	this.bufferMode = WriteShardBufferMode
+	this.bufferModeMux.Unlock()
+
+	// Calculate length
+	fileLen := uint32(len(b))
+
+	// Acquire write lock
+	this.contentsMux.Lock()
+
+	// Update metadata
+	f.Size = fileLen
+	f.StartOffset = this.contentsOffset
+
+	// Write contents to buffer
+	this.Contents().Write(b)
+
+	// Update content offset
+	this.contentsOffset += f.Size
+
+	// Unlock write
+	this.contentsMux.Unlock()
+
+	// Append meta
+	this.shardFileMeta.Add(f)
+
+	// Update metadata
+	this.shardMeta.mux.Lock()
+	this.shardMeta.FileCount++
+	newCount := this.shardMeta.FileCount
+	this.shardMeta.mux.Unlock()
+
+	// Update index
+	this.shardIndex.Add(f.FullName)
+
+	// Log
+	log.Infof("Created file %s with size %d in shard %s, now contains %d file(s)", f.FullName, f.Size, this.IdStr(), newCount)
+
+	// Done
+	return f, nil
+}
+
 func newShard(b *Block) *Shard {
 	return &Shard{
 		contents:      nil,
---
+++ shard_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewFile(t *testing.T) {
+	startApplication()
+
+	// Register on volume
+	b := datastore.NewBlock()
+
+	// Get shard
+	shard := b.DataShards[0]
+
+	// == FIRST FILE ==
+	// Fake file
+	fileBytes := []byte("Hello File")
+	fileMeta := newFileMeta("/images/robin/hello.txt")
+
+	// Add file
+	updatedFileMeta, err := shard.AddFile(fileMeta, fileBytes)
+	// No errors
+	if err != nil {
+		t.Error("Failed to add file")
+	}
+
+	// Validate size
+	if updatedFileMeta.Size != uint32(len(fileBytes)) {
+		t.Error("Failed size validation")
+	}
+
+	// Validate offset (first file in test)
+	if updatedFileMeta.StartOffset != 0 {
+		t.Error("Start offset must be 0 for first file")
+	}
+
+	// == SECOND FILE ==
+	// Fake another file
+	fileBytes2 := []byte("Hello number two")
+	fileMeta2 := newFileMeta("/images/robin/hello2.txt")
+
+	// Add another file
+	updatedFileMeta2, err2 := shard.AddFile(fileMeta2, fileBytes2)
+	if err2 != nil {
+		t.Error(err2)
+	}
+
+	// Offset of file 2 should be after file 1
+	if updatedFileMeta2.StartOffset != uint32(len(fileBytes)) {
+		t.Error("Should start after file 1")
+	}
+
+	// Validate shard meta
+	if shard.FileCount() != 2 {
+		t.Error("Shard should contain 2 files")
+	}
+
+	// Validate shard index
+	if shard.shardIndex.Test("/non-existing") == true {
+		t.Error("Shard index false positive")
+	}
+	if shard.shardIndex.Test(fileMeta.FullName) == false {
+		t.Error("Shard index does not contain file 1")
+	}
+	if shard.shardIndex.Test(fileMeta2.FullName) == false {
+		t.Error("Shard index does not contain file 2")
+	}
+
+	// Validate shard meta (de)serialization
+	shardFileMetaBytes := shard.shardFileMeta.Bytes()
+	if len(shardFileMetaBytes) < 1 {
+		panic("Shard file meta bytes must be non-zero")
+	}
+
+	// Load these bytes into new to confirm it's still intact
+	newShardFileMetaInstance := newShardFileMeta()
+	if len(newShardFileMetaInstance.fileMeta) != 0 {
+		panic("Empty shard file meta must be empty")
+	}
+	newShardFileMetaInstance.FromBytes(shardFileMetaBytes)
+	if len(newShardFileMetaInstance.fileMeta) != 2 {
+		panic("Loaded shard file meta must be 2")
+	}
+}
