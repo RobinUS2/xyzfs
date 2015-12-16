2132a0a27b3d10ffd53494054d6ab3b225859938
Robin Verlangen <robin@us2.nl>
2015-12-16 23:09:48 +0100

    Validate data shard integrity on erasure coding

--- block_test.go
+++ block_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
 	"testing"
 )
 
@@ -13,8 +15,25 @@ func TestNewBlock(t *testing.T) {
 	// Persist
 	b.Persist()
 
+	// Calculate hashes
+	preHash := make([]string, 10)
+	for i, ds := range b.DataShards {
+		h := md5.New()
+		h.Write(ds.Contents().Bytes())
+		preHash[i] = fmt.Sprintf("%x", h.Sum(nil))
+	}
+
 	// Encode
 	b.ErasureEncoding()
+
+	// Validate hashes after are equal, meaning the data has not changed
+	for i, ds := range b.DataShards {
+		h := md5.New()
+		h.Write(ds.Contents().Bytes())
+		if preHash[i] != fmt.Sprintf("%x", h.Sum(nil)) {
+			panic("Erasure encoding has changed file contents of data partition")
+		}
+	}
 
 	// Persist with encoding
 	b.Persist()
