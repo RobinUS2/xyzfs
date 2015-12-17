5dedffb9c64179d8536f1e744504dba71a9bc7f4
Robin Verlangen <robin@us2.nl>
2015-12-17 21:53:23 +0100

    Read and write gossip message

--- gossip.go
+++ gossip.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"sync"
+	"time"
 )
 
 // Responsible for figuring out which other nodes are out there
@@ -11,6 +13,9 @@ import (
 var gossip *Gossip
 
 type Gossip struct {
+	// Connections
+	connections    map[string]*net.Conn
+	connectionsMux sync.RWMutex
 }
 
 // Listen
@@ -44,14 +49,89 @@ func (this *Gossip) handleConnection(conn net.Conn) {
 			break
 		}
 		log.Infof("Bytes %v", tbuf[0:n])
+
+		// Read message
+		msg := &GossipMessage{}
+		msg.FromBytes(tbuf[0:n])
+		log.Infof("Gossip message %v", msg)
 	}
 }
 
+// Discover seeds
+func (this *Gossip) discoverSeeds() {
+	for _, seed := range conf.Seeds {
+		go this._connect(seed)
+	}
+}
+
+// Discover a single seed
+func (this *Gossip) _connect(node string) {
+	log.Infof("Contacting node %s", node)
+	var conn net.Conn
+	var err error
+	for {
+		conn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", node, conf.GossipPort))
+		if err != nil {
+			// handle error
+			log.Errorf("Failed to contact node %s: %s", node, err)
+
+			// Close connection
+			if conn != nil {
+				conn.Close()
+			}
+
+			// Wait a bit
+			time.Sleep(1 * time.Second)
+
+			// Try again
+			continue
+		}
+
+		log.Infof("Established connection with node %s", node)
+		break
+	}
+
+	// Keep connection
+	this.connectionsMux.Lock()
+	this.connections[node] = &conn
+	this.connectionsMux.Unlock()
+
+	// Send HELLO message
+	this._sendHello(node)
+}
+
+// Send hello message to node
+func (this *Gossip) _sendHello(node string) {
+	msg := newGossipMessage(HelloGossipMessageType, nil)
+	this._send(node, msg)
+}
+
+// Send message
+func (this *Gossip) _send(node string, msg *GossipMessage) {
+	// @todo rewrite, use a channel to buffer messages and write async in order to node
+	this.connectionsMux.Lock()
+	defer this.connectionsMux.Unlock()
+	if this.connections[node] == nil {
+		log.Warnf("No connection found to %s", node)
+		return
+	}
+	conn := *this.connections[node]
+	b := msg.Bytes()
+	conn.Write(b)
+	log.Debugf("Written %d gossip bytes to %s", len(b), node)
+}
+
+// New gossip service
 func newGossip() *Gossip {
-	g := &Gossip{}
+	g := &Gossip{
+		connections: make(map[string]*net.Conn),
+	}
 
 	// Start server
 	go g.listen()
+
+	// Start seed discovery
+	g.discoverSeeds()
 
 	return g
 }
--- gossip_message.go
+++ gossip_message.go
@@ -1,21 +1,67 @@
 package main
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 // Message to send to other nodes
 type GossipMessage struct {
-	Version           uint   // Message format (e.g. 1)
-	GossipMessageType uint   // Sort message
-	Len               uint   // Length of data
-	Data              []byte // Actual data
+	Version uint32            // Message format (e.g. 1)
+	Type    GossipMessageType // Sort message
+	Len     uint32            // Length of data
+	Data    []byte            // Actual data
 }
 
 // This version
-const GOSSIP_MESSAGE_VERSION uint = 1
+const GOSSIP_MESSAGE_VERSION uint32 = 1
 
 // Message type
-type GossipMessageType uint
+type GossipMessageType uint32
 
 // Message types
 const (
 	EmptyGossipMessageType     GossipMessageType = iota // 0 = not set
-	NodeStateGossipMessageType                          // 1 = node state changes
+	HelloGossipMessageType                              // 1 = initial hello message for handshake
+	NodeStateGossipMessageType                          // 2 = node state changes
 )
+
+// To bytes
+func (this *GossipMessage) Bytes() []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, this.Version) // meta version
+	binary.Write(buf, binary.BigEndian, this.Type)    // type
+	binary.Write(buf, binary.BigEndian, this.Len)     // data length
+	if this.Data != nil {
+		buf.Write(this.Data)
+	}
+	return buf.Bytes()
+}
+
+// From bytes
+func (this *GossipMessage) FromBytes(b []byte) {
+	buf := bytes.NewReader(b)
+	var err error
+	err = binary.Read(buf, binary.BigEndian, &this.Version) // meta version
+	panicErr(err)
+	err = binary.Read(buf, binary.BigEndian, &this.Type) // type
+	panicErr(err)
+	err = binary.Read(buf, binary.BigEndian, &this.Len) // length
+	panicErr(err)
+	this.Data = make([]byte, this.Len)
+	buf.Read(this.Data)
+}
+
+// New message
+func newGossipMessage(t GossipMessageType, data []byte) *GossipMessage {
+	var l uint32 = 0
+	if data != nil {
+		l = uint32(len(data))
+	}
+	return &GossipMessage{
+		Version: GOSSIP_MESSAGE_VERSION,
+		Type:    t,
+		Len:     l,
+		Data:    data,
+	}
+}
