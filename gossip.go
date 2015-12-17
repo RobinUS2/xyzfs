edfc91980172162594900ceb92ae912c5b02b257
Robin Verlangen <robin@us2.nl>
2015-12-17 22:12:45 +0100

    Abstracted transport

--- gossip.go
+++ gossip.go
@@ -1,103 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net"
-	"sync"
-	"time"
-)
-
 // Responsible for figuring out which other nodes are out there
 
 var gossip *Gossip
 
 type Gossip struct {
-	// Connections
-	connections    map[string]*net.Conn
-	connectionsMux sync.RWMutex
-}
-
-// Listen
-func (this *Gossip) listen() {
-	log.Infof("Starting gossip on port TCP/%d", conf.GossipPort)
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", conf.GossipPort))
-	if err != nil {
-		panicErr(err)
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			// handle error
-		}
-		go this.handleConnection(conn)
-	}
-}
-
-// Handle connection
-func (this *Gossip) handleConnection(conn net.Conn) {
-	// Read bytes
-	tbuf := make([]byte, 8096)
-
-	for {
-		n, err := conn.Read(tbuf)
-		// Was there an error in reading ?
-		if err != nil {
-			if err != io.EOF {
-				log.Printf("Read error: %s", err)
-			}
-			break
-		}
-		log.Infof("Bytes %v", tbuf[0:n])
-
-		// Read message
-		msg := &GossipMessage{}
-		msg.FromBytes(tbuf[0:n])
-		log.Infof("Gossip message %v", msg)
-	}
+	transport *NetworkTransport
 }
 
 // Discover seeds
 func (this *Gossip) discoverSeeds() {
 	for _, seed := range conf.Seeds {
-		go this._connect(seed)
+		go this.transport._connect(seed)
 	}
-}
-
-// Discover a single seed
-func (this *Gossip) _connect(node string) {
-	log.Infof("Contacting node %s", node)
-	var conn net.Conn
-	var err error
-	for {
-		conn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", node, conf.GossipPort))
-		if err != nil {
-			// handle error
-			log.Errorf("Failed to contact node %s: %s", node, err)
-
-			// Close connection
-			if conn != nil {
-				conn.Close()
-			}
-
-			// Wait a bit
-			time.Sleep(1 * time.Second)
-
-			// Try again
-			continue
-		}
-
-		log.Infof("Established connection with node %s", node)
-		break
-	}
-
-	// Keep connection
-	this.connectionsMux.Lock()
-	this.connections[node] = &conn
-	this.connectionsMux.Unlock()
-
-	// Send HELLO message
-	this._sendHello(node)
 }
 
 // Send hello message to node
@@ -108,30 +23,30 @@ func (this *Gossip) _sendHello(node string) {
 
 // Send message
 func (this *Gossip) _send(node string, msg *GossipMessage) {
-	// @todo rewrite, use a channel to buffer messages and write async in order to node
-	this.connectionsMux.Lock()
-	defer this.connectionsMux.Unlock()
-	if this.connections[node] == nil {
-		log.Warnf("No connection found to %s", node)
-		return
-	}
-	conn := *this.connections[node]
-	b := msg.Bytes()
-	conn.Write(b)
-	log.Debugf("Written %d gossip bytes to %s", len(b), node)
+	this.transport._send(node, msg.Bytes())
 }
 
 // New gossip service
 func newGossip() *Gossip {
+	// Create gossip
 	g := &Gossip{
-		connections: make(map[string]*net.Conn),
+		transport: newNetworkTransport("gossip", conf.GossipPort),
 	}
 
-	// Start server
-	go g.listen()
+	// Message
+	g.transport._onMessage = func(b []byte) {
+		msg := &GossipMessage{}
+		msg.FromBytes(b)
+		log.Infof("%s message %v", g.transport.serviceName, msg)
+	}
 
-	// Start seed discovery
-	g.discoverSeeds()
+	// Connect
+	g.transport._onConnect = func(node string) {
+		g._sendHello(node)
+	}
+
+	// Start
+	g.transport.start()
 
 	return g
 }
---
+++ transport.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+// Network transport layer
+
+type NetworkTransport struct {
+	port        int
+	serviceName string
+	// Handlers
+	_onMessage func([]byte)
+	_onConnect func(string)
+	// Connections
+	connections    map[string]*net.Conn
+	connectionsMux sync.RWMutex
+}
+
+// Listen
+func (this *NetworkTransport) listen() {
+	log.Infof("Starting %s on port TCP/%d", this.serviceName, this.port)
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", this.port))
+	if err != nil {
+		panicErr(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go this.handleConnection(conn)
+	}
+}
+
+// Handle connection
+func (this *NetworkTransport) handleConnection(conn net.Conn) {
+	// Read bytes
+	tbuf := make([]byte, 8096)
+
+	for {
+		n, err := conn.Read(tbuf)
+		// Was there an error in reading ?
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("Read error: %s", err)
+			}
+			break
+		}
+		log.Infof("Bytes %v", tbuf[0:n])
+
+		// Read message
+		this._onMessage(tbuf[0:n])
+	}
+}
+
+// Discover seeds
+func (this *NetworkTransport) discoverSeeds() {
+	for _, seed := range conf.Seeds {
+		go this._connect(seed)
+	}
+}
+
+// Discover a single seed
+func (this *NetworkTransport) _connect(node string) {
+	log.Infof("Contacting node %s for %s", node, this.serviceName)
+	var conn net.Conn
+	var err error
+	for {
+		conn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", node, this.port))
+		if err != nil {
+			// handle error
+			log.Errorf("Failed to contact node %s for %s: %s", node, this.serviceName, err)
+
+			// Close connection
+			if conn != nil {
+				conn.Close()
+			}
+
+			// Wait a bit
+			time.Sleep(1 * time.Second)
+
+			// Try again
+			continue
+		}
+
+		log.Infof("Established connection with node %s for %s", node, this.serviceName)
+		break
+	}
+
+	// Keep connection
+	this.connectionsMux.Lock()
+	this.connections[node] = &conn
+	this.connectionsMux.Unlock()
+
+	// Send HELLO message
+	this._onConnect(node)
+}
+
+// Send message
+func (this *NetworkTransport) _send(node string, b []byte) {
+	// @todo rewrite, use a channel to buffer messages and write async in order to node
+	this.connectionsMux.Lock()
+	defer this.connectionsMux.Unlock()
+	if this.connections[node] == nil {
+		log.Warnf("No connection found to %s for %s", node, this.serviceName)
+		return
+	}
+	conn := *this.connections[node]
+	conn.Write(b)
+	log.Debugf("Written %d %s bytes to %s", len(b), this.serviceName, node)
+}
+
+// Start
+func (this *NetworkTransport) start() {
+	// Start server
+	go this.listen()
+
+	// Start seed discovery
+	this.discoverSeeds()
+}
+
+// New NetworkTransport service
+func newNetworkTransport(serviceName string, port int) *NetworkTransport {
+	g := &NetworkTransport{
+		port:        port,
+		serviceName: serviceName,
+		connections: make(map[string]*net.Conn),
+	}
+
+	return g
+}
