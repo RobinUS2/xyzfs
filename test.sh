6776e0a6a69b055faf733ebca8c7ecb5ed18f279
Robin Verlangen <robin@us2.nl>
2015-12-27 14:17:10 +0100

    Improve test script

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Build
-source ./build.sh race
-
-# Run tests
-go get golang.org/x/tools/cmd/cover
-go test -cover -bench -v .
-
 # Run vet
 go vet .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+# Build
+source ./build.sh race
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+# Test deps
+go get golang.org/x/tools/cmd/cover
+
+# Run tests and benchmark
+go test -cover -bench -v .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
